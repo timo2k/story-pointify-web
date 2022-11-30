367ae2780ba31efd4700693b2fe7772e8db921da
Timo Ischen <info@timo-ischen.de>
2022-11-30 17:52:13 +0100

    fix: Add upgrade insecure request meta data

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,12 @@ import { Html, Head, Main, NextScript } from 'next/document';
 const Document = () => {
   return (
     <Html className="h-full bg-gray-50">
-      <Head />
+      <Head>
+        <meta
+          httpEquiv="Content-Security-Policy"
+          content="upgrade-insecure-requests"
+        />
+      </Head>
       <body className="h-full">
         <Main />
         <NextScript />
